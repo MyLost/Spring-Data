1fd0686e92f74e3588d2c5b1f39f793e27efa51d
darthReader <ddimitrov10@gmail.com>
2022-10-23 09:01:12 +0300

    workshop

--- SimpleRetrievalDataExercices/src/lecture/MainDiablo.java
+++ SimpleRetrievalDataExercices/src/lecture/MainDiablo.java
@@ -1,3 +1,5 @@
+package lecture;
+
 import java.sql.*;
 import java.util.Properties;
 import java.util.Scanner;
@@ -6,39 +8,54 @@ import java.util.Scanner;
  *  Program retrieves simple data from database diablo via java code.
  *  User, password and username are read from console.
  *  You need MySql JDBC driver library to can run this example!
+ *
+ *
  */
 public class MainDiablo {
+
+    private static final String JDBC_CONNECTION_STRING = "jdbc:mysql://localhost:3306/diablo";
+
+    private static final String ENTER_DATABASE_USER = "Enter database user:";
+
+    private static final String ENTER_DATABASE_USER_PASSWORD = "Enter password for database user %s:";
+
+    private static final String ENTER_USERNAME = "Please enter username to retrieve data";
+
+    private static final String QUERY = "select user_name ,first_name , last_name , count(user_id) as `count` \n" +
+            "from users u \n" +
+            "join users_games ug on ug.user_id = u.id \n" +
+            "where user_name = ? \n" +
+            "group by user_id;";
+
+    private static final String NO_SUCH_USER = "No such user exists";
+
     public static void main(String[] args) throws SQLException {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter database user:");
+        System.out.println(ENTER_DATABASE_USER);
         String user = scanner.nextLine();
 
-        System.out.println(String.format("Enter password for database user %s:", user));
+        System.out.println(String.format(ENTER_DATABASE_USER_PASSWORD, user));
         String password = scanner.nextLine();
 
         Properties props = new Properties();
         props.setProperty("user", user);
         props.setProperty("password", password);
 
-        System.out.println(props.toString());
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/diablo", props);
+        System.out.println(props);
+        Connection connection = DriverManager.getConnection(JDBC_CONNECTION_STRING, props);
 
-        System.out.println("Please enter username to retrieve data");
+        System.out.println(ENTER_USERNAME);
         String username = scanner.nextLine();
 
-        PreparedStatement statement = connection.prepareStatement("select user_name ,first_name , last_name , count(user_id) as `count` \n" +
-                "from users u \n" +
-                "join users_games ug on ug.user_id = u.id \n" +
-                "where user_name = ? \n" +
-                "group by user_id;");
+        PreparedStatement statement = connection.prepareStatement(QUERY);
 
         statement.setString(1, username);
 
         ResultSet result = statement.executeQuery();
 
         if(!result.next()) {
-            System.out.println("No such user exists");
+            System.out.println(NO_SUCH_USER);
         } else {
             while(result.next()) {
                 StringBuilder builder = new StringBuilder();

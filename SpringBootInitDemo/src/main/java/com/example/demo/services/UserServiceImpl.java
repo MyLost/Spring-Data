772b9b1d44496030caba160b26804962f93bb970
darthReader <ddimitrov10@gmail.com>
2022-11-03 07:55:11 +0200

    introduction to spring data lab!

---
+++ SpringBootInitDemo/src/main/java/com/example/demo/services/UserServiceImpl.java
@@ -0,0 +1,42 @@
+package com.example.demo.services;
+
+import com.example.demo.models.Account;
+import com.example.demo.models.User;
+import com.example.demo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void register(String username, int age) {
+        if(username.isBlank() || age < 18) {
+            throw new RuntimeException("Validation failed");
+        }
+
+        Optional<User> byUsername = this.userRepository.findByUsername(username);
+
+        if(byUsername.isPresent()) {
+            throw new RuntimeException("Username already in use");
+        }
+
+        Account account = new Account();
+
+        User user = new User();
+        user.setUsername(username);
+        user.setAge(age);
+        user.addAccounts(account);
+
+        this.userRepository.save(user);
+    }
+}

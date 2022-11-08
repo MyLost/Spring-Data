844019611c4f44a4ddafcfae3dd096aa081edbbe
darthReader <ddimitrov10@gmail.com>
2022-11-08 17:10:19 +0200

    Hibernate code first approach exercises

---
+++ HibernateCodeFirstEntityRelations/src/main/java/entities/paymentSystem/BaseEntity.java
@@ -0,0 +1,16 @@
+package entities.PaymentSystem;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private long id;
+}

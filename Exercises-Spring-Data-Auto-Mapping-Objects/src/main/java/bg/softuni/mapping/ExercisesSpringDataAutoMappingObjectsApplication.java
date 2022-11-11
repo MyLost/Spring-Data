cf9011f89b924c88d967baa5a668bf569b96ad32
darthReader <ddimitrov10@gmail.com>
2022-11-11 08:32:27 +0200

    initial commit

---
+++ Exercises-Spring-Data-Auto-Mapping-Objects/src/main/java/bg/softuni/mapping/ExercisesSpringDataAutoMappingObjectsApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.mapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercisesSpringDataAutoMappingObjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercisesSpringDataAutoMappingObjectsApplication.class, args);
+	}
+
+}

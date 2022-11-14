da268d6c9b1acd3ca63233866e3ac3b32e0e39ed
darthReader <ddimitrov10@gmail.com>
2022-11-14 12:00:20 +0200

    task enhanced with new classes!

---
+++ Exercises-Spring-Data-Auto-Mapping-Objects/src/main/java/bg/softuni/mapping/dtos/UserRegister.java
@@ -0,0 +1,53 @@
+package bg.softuni.mapping.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.regex.Pattern;
+
+import static bg.softuni.mapping.constants.Validations.*;
+
+@Data
+@NoArgsConstructor
+@Builder
+public class UserRegister {
+
+    private String email;
+
+    private String password;
+
+    private String confirmPassword;
+
+    private String fullName;
+
+    public UserRegister(String email, String password, String confirmPassword, String fullName) {
+        this.email = email;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+        this.fullName = fullName;
+        validate();
+    }
+
+    private void validate() {
+        boolean isEmailValid = Pattern.matches(EMAIL_PATTERN, this.email);
+
+        //validate email
+        if(!isEmailValid) {
+            throw new IllegalArgumentException(EMAIL_NOT_VALID_MESSAGE);
+        }
+
+        boolean isPasswordValid = Pattern.matches(PASSWORD_PATTERN, this.password);
+
+        //validate password
+        if(!isPasswordValid) {
+            throw new IllegalArgumentException(PASSWORD_NOT_VALID_MESSAGE);
+        }
+
+        // check password and confirm password match
+        if(!password.equals(confirmPassword)) {
+            throw new IllegalArgumentException(PASSWORD_MISS_MATCH_MESSAGE);
+        }
+    }
+}

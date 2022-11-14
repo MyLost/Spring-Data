54a22f17b3ba45bb614a56f102c0faff6269ca6b
darthReader <ddimitrov10@gmail.com>
2022-11-14 12:00:02 +0200

    task enhanced with new classes!

---
+++ Exercises-Spring-Data-Auto-Mapping-Objects/src/main/java/bg/softuni/mapping/repository/GameRepository.java
@@ -0,0 +1,9 @@
+package bg.softuni.mapping.repository;
+
+import bg.softuni.mapping.entities.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GameRepository extends JpaRepository<Game, Long> {
+}

84705484b1ca8481dcd3cf13382fec535e7e3908
darthReader <ddimitrov10@gmail.com>
2022-11-17 17:40:17 +0200

    task enhanced with new classes!

--- Exercises-Spring-Data-Auto-Mapping-Objects/src/main/java/bg/softuni/mapping/constants/Commands.java
+++ Exercises-Spring-Data-Auto-Mapping-Objects/src/main/java/bg/softuni/mapping/constants/Commands.java
@@ -3,4 +3,19 @@ package bg.softuni.mapping.constants;
 public enum Commands {
     ;
     public final static String REGISTER_USER = "RegisterUser";
+
+    public static final String LOGIN = "LoginUser";
+    public static final String LOGOUT = "Logout";
+
+    public static final String ADD_GAME = "AddGame";
+
+    public static final String EDIT_GAME = "EditGame";
+
+    public static final String DELETE_GAME = "DeleteGame";
+
+    public static final String ALL_GAMES = "AllGames";
+
+    public static final String DETAIL_GAME = "DetailGame";
+
+    public static final String OWNED_GAMES = "OwnedGames";
 }

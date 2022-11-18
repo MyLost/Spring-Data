a4bf1e3d804649bc8e2b9b65b3e6e9d7c3f29b3e
darthReader <ddimitrov10@gmail.com>
2022-11-18 11:36:26 +0200

    task enhanced with new classes!

---
+++ Exercises-Spring-Data-Auto-Mapping-Objects/src/main/java/bg/softuni/mapping/dtos/DetailsGameDto.java
@@ -0,0 +1,13 @@
+package bg.softuni.mapping.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class DetailsGameDto {
+
+    private String gameTitle;
+}

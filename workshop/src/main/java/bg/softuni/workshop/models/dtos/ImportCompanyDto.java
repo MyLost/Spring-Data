4f511b279754973fcc3e53579eb190307c3e183f
darthReader <ddimitrov10@gmail.com>
2022-11-29 08:51:57 +0200

    workshop

---
+++ workshop/src/main/java/bg/softuni/workshop/models/dtos/ImportCompanyDto.java
@@ -0,0 +1,21 @@
+package bg.softuni.workshop.models.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name="company")
+@XmlAccessorType(XmlAccessType.FIELD)
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ImportCompanyDto {
+
+    @XmlElement(name = "name")
+    private String name;
+}

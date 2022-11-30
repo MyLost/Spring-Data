3be06b0d5edaff2bc9c6de2ee4acb13e7d04a421
darthReader <ddimitrov10@gmail.com>
2022-11-30 17:19:29 +0200

    xml exercises

---
+++ xmlProccessing/src/main/java/bg/softuni/xmlProccessing/domain/dtos/importDto/CustomerImportDto.java
@@ -0,0 +1,31 @@
+package bg.softuni.xmlProccessing.domain.dtos.importDto;
+
+import java.time.LocalDateTime;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+
+import bg.softuni.xmlProccessing.util.LocalDateTimeAdapter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@XmlRootElement(name = "customer")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class CustomerImportDto {
+    @XmlAttribute(name = "name")
+    private String name;
+    @XmlElement(name = "birth-date")
+    @XmlJavaTypeAdapter(LocalDateTimeAdapter.class)
+    private LocalDateTime birthDate;
+    @XmlElement(name = "is-young-driver")
+    private Boolean isYoungDriver;
+}

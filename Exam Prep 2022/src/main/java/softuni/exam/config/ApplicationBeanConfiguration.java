ddf778535ac0fde66cf49f0dfe1131975adfe578
darthReader <ddimitrov10@gmail.com>
2022-12-04 10:19:00 +0200

    WIP

--- Exam Prep 2022/src/main/java/softuni/exam/config/ApplicationBeanConfiguration.java
+++ Exam Prep 2022/src/main/java/softuni/exam/config/ApplicationBeanConfiguration.java
@@ -1,6 +1,29 @@
 package softuni.exam.config;
 
-// TODO:
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class ApplicationBeanConfiguration {
 
+    @Bean
+    public Gson gson() {
+        return new GsonBuilder().setPrettyPrinting().create();
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        final ModelMapper modelMapper = new ModelMapper();
+
+        modelMapper.addConverter(mappingContext ->
+                        LocalTime.parse(mappingContext.getSource(), DateTimeFormatter.ofPattern("HH:mm:ss")),
+                String.class,
+                LocalTime.class);
+
+        return modelMapper;
+    }
 }

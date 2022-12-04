167dd79b69314ed8110b905e6b834c7453da3391
darthReader <ddimitrov10@gmail.com>
2022-12-04 10:21:17 +0200

    WIP

--- Exercises-Spring-Data-Auto-Mapping-Objects/src/main/java/bg/softuni/mapping/repository/GameRepository.java
+++ Exercises-Spring-Data-Auto-Mapping-Objects/src/main/java/bg/softuni/mapping/repository/GameRepository.java
@@ -1,9 +1,13 @@
 package bg.softuni.mapping.repository;
 
-import bg.softuni.mapping.entities.Game;
+import bg.softuni.mapping.entities.GameEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface GameRepository extends JpaRepository<Game, Long> {
+public interface GameRepository extends JpaRepository<GameEntity, Long> {
+
+    Optional<GameEntity> findByTitle(String title);
 }

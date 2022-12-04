e958d97075019a42f7e99d5be7673ca4ad5a8bdb
darthReader <ddimitrov10@gmail.com>
2022-12-04 10:20:51 +0200

    WIP

--- Exam Prep 2022/src/main/java/softuni/exam/repository/ForecastRepository.java
+++ Exam Prep 2022/src/main/java/softuni/exam/repository/ForecastRepository.java
@@ -1,6 +1,15 @@
 package softuni.exam.repository;
 
-// TODO:
-public interface ForecastRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import softuni.exam.models.entity.Forecast;
 
+import java.time.DayOfWeek;
+import java.util.Optional;
+import java.util.Set;
+
+@Repository
+public interface ForecastRepository extends JpaRepository<Forecast, Long> {
+
+    Optional<Set<Forecast>> findAllByDayOfWeekAndCity_PopulationLessThanOrderByMaxTemperatureDescIdAsc(DayOfWeek day, Long num);
 }

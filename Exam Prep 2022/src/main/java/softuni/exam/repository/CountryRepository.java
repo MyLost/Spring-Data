a91d7a91e37b0e4c6a92f673546c9bb255206826
darthReader <ddimitrov10@gmail.com>
2022-12-04 10:19:42 +0200

    WIP

--- Exam Prep 2022/src/main/java/softuni/exam/repository/CountryRepository.java
+++ Exam Prep 2022/src/main/java/softuni/exam/repository/CountryRepository.java
@@ -1,6 +1,16 @@
 package softuni.exam.repository;
 
-// TODO:
-public interface CountryRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import softuni.exam.models.entity.Country;
+
+import java.util.Optional;
+
+@Repository
+public interface CountryRepository extends JpaRepository<Country, Long> {
+
+    Optional<Country> findFirstByCountryName(String name);
+
+    Optional<Country> findById(Long id);
 
 }

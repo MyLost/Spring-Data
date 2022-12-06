b65b8fd76c36a48b92f4aa082288e389683d410c
darthReader <ddimitrov10@gmail.com>
2022-12-06 08:17:44 +0200

    task enhancement

--- SpringDataAdvancedQueringExercices/src/main/java/com/example/springintro/repository/AuthorRepository.java
+++ SpringDataAdvancedQueringExercices/src/main/java/com/example/springintro/repository/AuthorRepository.java
@@ -6,10 +6,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 
-    @Query("SELECT a FROM Author a ORDER BY a.books.size DESC")
+    String SELECT_A_FROM_AUTHOR_A_ORDER_BY_A_BOOKS_SIZE_DESC = "SELECT a FROM Author a ORDER BY a.books.size DESC";
+
+    @Query(SELECT_A_FROM_AUTHOR_A_ORDER_BY_A_BOOKS_SIZE_DESC)
     List<Author> findAllByBooksSizeDESC();
+
+    Optional<List<Author>> findAllByFirstNameEndingWith(String suffix);
 }

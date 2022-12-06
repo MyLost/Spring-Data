619006544174a55e86c8fa5a2d253228b5c3398e
darthReader <ddimitrov10@gmail.com>
2022-12-06 08:18:36 +0200

    task enhancement

---
+++ JsonProccessing/src/main/java/bg/softuni/json/entities/BaseEntity.java
@@ -0,0 +1,21 @@
+package bg.softuni.json.entities;
+
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+}

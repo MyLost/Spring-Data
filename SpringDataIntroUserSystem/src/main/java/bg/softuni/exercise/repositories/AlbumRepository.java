1f9500c112662207aad8cc1e81c5e15c1bbcf356
darthReader <ddimitrov10@gmail.com>
2022-12-09 10:16:05 +0200

    task enhancement

---
+++ SpringDataIntroUserSystem/src/main/java/bg/softuni/exercise/repositories/AlbumRepository.java
@@ -0,0 +1,9 @@
+package bg.softuni.exercise.repositories;
+
+import bg.softuni.exercise.entities.Album;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlbumRepository extends JpaRepository<Album, Long> {
+}

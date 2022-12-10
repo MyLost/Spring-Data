c160c4f81faa72b1030b7c7b9451d107fc0578e1
darthReader <ddimitrov10@gmail.com>
2022-12-10 09:29:27 +0200

    task enhancement

---
+++ JsonProccessing/src/main/java/bg/softuni/json/dtos/category/CategoryCountProductsDto.java
@@ -0,0 +1,19 @@
+package bg.softuni.json.dtos.category;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class CategoryCountProductsDto {
+    private String category;
+    private Long productsCount;
+    private Double averagePrice;
+    private BigDecimal totalRevenue;
+}

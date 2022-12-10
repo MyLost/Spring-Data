252400b9571bd50d992ef0f60f0b0fca1026aa62
darthReader <ddimitrov10@gmail.com>
2022-12-10 09:31:17 +0200

    task enhancement

---
+++ xmlProccessing/src/main/java/bg/softuni/xmlProccessing/domain/dtos/exportDto/CustomerWithSalesWrapperDto.java
@@ -0,0 +1,25 @@
+package bg.softuni.xmlProccessing.domain.dtos.exportDto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@XmlRootElement(name = "customers")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class CustomerWithSalesWrapperDto {
+    @XmlElement(name = "customer")
+    private List<CustomerWithSalesDto> customers;
+
+    public CustomerWithSalesWrapperDto() {
+        customers = new ArrayList<CustomerWithSalesDto>();
+    }
+}

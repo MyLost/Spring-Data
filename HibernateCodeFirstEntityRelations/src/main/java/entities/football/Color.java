50973d8628111dc43049b6c0ab51bceaaa49f9df
darthReader <ddimitrov10@gmail.com>
2022-12-10 09:30:16 +0200

    task enhancement

---
+++ HibernateCodeFirstEntityRelations/src/main/java/entities/football/Color.java
@@ -0,0 +1,21 @@
+package entities.football;
+
+import entities.football.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table
+public class Color extends BaseEntity {
+
+    @Column
+    private String name;
+
+}

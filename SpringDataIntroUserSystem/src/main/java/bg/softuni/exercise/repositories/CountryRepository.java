96eb08df1ce48225f06f4982829a0e7b0ba6f5b6
darthReader <ddimitrov10@gmail.com>
2022-12-10 09:31:07 +0200

    task enhancement

---
+++ SpringDataIntroUserSystem/src/main/java/bg/softuni/exercise/repositories/CountryRepository.java
@@ -0,0 +1,9 @@
+package bg.softuni.exercise.repositories;
+
+import bg.softuni.exercise.entities.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CountryRepository extends JpaRepository<Country, Long> {
+}

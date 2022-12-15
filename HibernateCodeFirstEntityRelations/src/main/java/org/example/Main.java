5fe795b64d13123221bcb2d4ba1b8b90f2207c95
darthReader <ddimitrov10@gmail.com>
2022-12-15 18:36:50 +0200

    task enhancement

--- HibernateCodeFirstEntityRelations/src/main/java/org/example/Main.java
+++ HibernateCodeFirstEntityRelations/src/main/java/org/example/Main.java
@@ -14,66 +14,68 @@ import java.time.LocalDate;
     Project use project lombok for generating getter, setters and constructors!
     Entities for each task are in different package!
     Entity manager is obtained from local class named Connection!
+
+    First Task for gringotts not require relations, but relation are used in this example!
  */
 
 public class Main {
     public static void main(String[] args) {
 
-        EntityManager entityManager = new Connection("example-gringotts").getEntityManager();
+        EntityManager entityManager = new Connection("example-sales").getEntityManager();
 
         entityManager.getTransaction().begin();
-
-        GringottsEntity gringottOne = new GringottsEntity();
-        gringottOne.setAge(20);
-        gringottOne.setFirstName("Gringo");
-        gringottOne.setLastName("Gringov");
-        gringottOne.setNotes("This is some awesome gringott!");
-
-        MagicWandEntity magicWandForGringottOne = new MagicWandEntity();
-        magicWandForGringottOne.setMagicWandCreator("Alex");
-        magicWandForGringottOne.setMagicWandSize(35);
-
-        entityManager.persist(magicWandForGringottOne);
-
-        gringottOne.setMagicWand(magicWandForGringottOne);
-        entityManager.persist(gringottOne);
-
-        GringottDepositsEntity gringottOneDeposit = new GringottDepositsEntity();
-        gringottOneDeposit.setDepositAmount(500.00);
-        gringottOneDeposit.setDepositCharge(300.00);
-        gringottOneDeposit.setGringott(gringottOne);
-        gringottOneDeposit.setDepositGroup("Gringotts");
-        gringottOneDeposit.setIsDepositExpired(false);
-        gringottOneDeposit.setDepositInterest(100.00);
-        gringottOneDeposit.setDepositExpirationDate(LocalDate.of(2002,12,31));
-        gringottOneDeposit.setDepositStartDate(LocalDate.now());
-
-        entityManager.persist(gringottOneDeposit);
-
-        GringottDepositsEntity gringottOneSecondDeposit = new GringottDepositsEntity();
-        gringottOneSecondDeposit.setDepositAmount(400.00);
-        gringottOneSecondDeposit.setDepositCharge(200.00);
-        gringottOneSecondDeposit.setGringott(gringottOne);
-        gringottOneSecondDeposit.setDepositGroup("Gringotts");
-        gringottOneSecondDeposit.setIsDepositExpired(false);
-        gringottOneSecondDeposit.setDepositInterest(50.00);
-        gringottOneSecondDeposit.setDepositExpirationDate(LocalDate.of(2002,12,31));
-        gringottOneSecondDeposit.setDepositStartDate(LocalDate.now());
-
-        entityManager.persist(gringottOneSecondDeposit);
-
-        GringottDepositsEntity gringottOneThirdDeposit = new GringottDepositsEntity();
-        gringottOneThirdDeposit.setDepositAmount(700.00);
-        gringottOneThirdDeposit.setDepositCharge(400.00);
-        gringottOneThirdDeposit.setGringott(gringottOne);
-        gringottOneThirdDeposit.setDepositGroup("Gringotts");
-        gringottOneThirdDeposit.setIsDepositExpired(false);
-        gringottOneThirdDeposit.setDepositInterest(60.00);
-        gringottOneThirdDeposit.setDepositExpirationDate(LocalDate.of(2002,12,31));
-        gringottOneThirdDeposit.setDepositStartDate(LocalDate.now());
-
-        entityManager.persist(gringottOneThirdDeposit);
-
+//
+//        GringottsEntity gringottOne = new GringottsEntity();
+//        gringottOne.setAge(20);
+//        gringottOne.setFirstName("Gringo");
+//        gringottOne.setLastName("Gringov");
+//        gringottOne.setNotes("This is some awesome gringott!");
+//
+//        MagicWandEntity magicWandForGringottOne = new MagicWandEntity();
+//        magicWandForGringottOne.setMagicWandCreator("Alex");
+//        magicWandForGringottOne.setMagicWandSize(35);
+//
+//        entityManager.persist(magicWandForGringottOne);
+//
+//        gringottOne.setMagicWand(magicWandForGringottOne);
+//        entityManager.persist(gringottOne);
+//
+//        GringottDepositsEntity gringottOneDeposit = new GringottDepositsEntity();
+//        gringottOneDeposit.setDepositAmount(500.00);
+//        gringottOneDeposit.setDepositCharge(300.00);
+//        gringottOneDeposit.setGringott(gringottOne);
+//        gringottOneDeposit.setDepositGroup("Gringotts");
+//        gringottOneDeposit.setIsDepositExpired(false);
+//        gringottOneDeposit.setDepositInterest(100.00);
+//        gringottOneDeposit.setDepositExpirationDate(LocalDate.of(2002,12,31));
+//        gringottOneDeposit.setDepositStartDate(LocalDate.now());
+//
+//        entityManager.persist(gringottOneDeposit);
+//
+//        GringottDepositsEntity gringottOneSecondDeposit = new GringottDepositsEntity();
+//        gringottOneSecondDeposit.setDepositAmount(400.00);
+//        gringottOneSecondDeposit.setDepositCharge(200.00);
+//        gringottOneSecondDeposit.setGringott(gringottOne);
+//        gringottOneSecondDeposit.setDepositGroup("Gringotts");
+//        gringottOneSecondDeposit.setIsDepositExpired(false);
+//        gringottOneSecondDeposit.setDepositInterest(50.00);
+//        gringottOneSecondDeposit.setDepositExpirationDate(LocalDate.of(2002,12,31));
+//        gringottOneSecondDeposit.setDepositStartDate(LocalDate.now());
+//
+//        entityManager.persist(gringottOneSecondDeposit);
+//
+//        GringottDepositsEntity gringottOneThirdDeposit = new GringottDepositsEntity();
+//        gringottOneThirdDeposit.setDepositAmount(700.00);
+//        gringottOneThirdDeposit.setDepositCharge(400.00);
+//        gringottOneThirdDeposit.setGringott(gringottOne);
+//        gringottOneThirdDeposit.setDepositGroup("Gringotts");
+//        gringottOneThirdDeposit.setIsDepositExpired(false);
+//        gringottOneThirdDeposit.setDepositInterest(60.00);
+//        gringottOneThirdDeposit.setDepositExpirationDate(LocalDate.of(2002,12,31));
+//        gringottOneThirdDeposit.setDepositStartDate(LocalDate.now());
+//
+//        entityManager.persist(gringottOneThirdDeposit);
+//
         entityManager.getTransaction().commit();
         entityManager.close();
     }

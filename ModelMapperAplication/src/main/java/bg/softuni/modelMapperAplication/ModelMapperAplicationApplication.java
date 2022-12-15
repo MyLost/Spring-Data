28e36d34b63237791990f2e4166adbea9d409264
darthReader <ddimitrov10@gmail.com>
2022-12-15 18:37:39 +0200

    task enhancement

---
+++ ModelMapperAplication/src/main/java/bg/softuni/modelMapperAplication/ModelMapperAplicationApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.modelMapperAplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelMapperAplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelMapperAplicationApplication.class, args);
+	}
+
+}

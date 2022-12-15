11a0ac2759d67aae3a33bffd54b19430ee0727d9
darthReader <ddimitrov10@gmail.com>
2022-12-15 18:36:22 +0200

    task enhancement

---
+++ JsonProccessing/src/main/java/bg/softuni/json/JsonProccessingApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonProccessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonProccessingApplication.class, args);
+	}
+
+}

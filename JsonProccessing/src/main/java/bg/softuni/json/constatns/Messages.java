64f9dc4b5d2aca66911743b6c2dd5f5ab8d99bca
darthReader <ddimitrov10@gmail.com>
2022-12-15 18:37:34 +0200

    task enhancement

---
+++ JsonProccessing/src/main/java/bg/softuni/json/constatns/Messages.java
@@ -0,0 +1,15 @@
+package bg.softuni.json.constatns;
+
+public class Messages {
+
+    public static final String NO_SUCH_MENU = "No such menu";
+    public static final String END_MENU = "End";
+
+    public static final String EXIT_PROGRAM = "You are exit the program";
+    public static final String LAST_NAME_TO_SHORT = "The last name of user is too short, must be minimum 3 characters";
+    public static final String NAME_CONSTAINT = "The name is too short, must be minimum 3 characters or  too long, must be maximum 15 characters";
+
+    public static final String BOTTOM_PRICE_RANGE = "Enter the bottom of the price range:";
+    public static final String TOP_PRICE_RANGE = "Enter the top of the price range";
+    public static final String CHECK_THE_FILE = "The data save to the file";
+}

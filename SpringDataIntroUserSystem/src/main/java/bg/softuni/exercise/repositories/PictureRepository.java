d0137684b0de2c992e54713b85509391fe9d05f3
darthReader <ddimitrov10@gmail.com>
2022-12-18 16:14:46 +0200

    task enhancement

---
+++ SpringDataIntroUserSystem/src/main/java/bg/softuni/exercise/repositories/PictureRepository.java
@@ -0,0 +1,9 @@
+package bg.softuni.exercise.repositories;
+
+import bg.softuni.exercise.entities.Picture;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PictureRepository extends JpaRepository<Picture, Long> {
+}

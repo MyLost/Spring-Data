03647bf45e410df9ccdbf078e233a5f27e1f2c4c
darthReader <ddimitrov10@gmail.com>
2022-12-18 16:14:01 +0200

    task enhancement

---
+++ xmlProccessing/src/main/java/bg/softuni/xmlProccessing/constant/Path.java
@@ -0,0 +1,11 @@
+package bg.softuni.xmlProccessing.constant;
+
+public enum Path {
+    ;
+    private static final String BASE_PATH = "xmlProccessing/src/main/resources/files";
+    public static final String CARS_XML =  BASE_PATH + "/cars.xml";
+    public static final String CUSTOMER_XML =  BASE_PATH + "/customers.xml";
+    public static final String PARTS_XML =  BASE_PATH + "/parts.xml";
+    public static final String SUPPLIER_XML = BASE_PATH + "/suppliers.xml";
+
+}

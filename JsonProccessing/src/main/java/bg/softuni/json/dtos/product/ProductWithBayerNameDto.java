2f52e2fa377445e613f150b6c4edb97233c617b8
darthReader <ddimitrov10@gmail.com>
2022-12-19 14:35:05 +0200

    task enhancement

---
+++ JsonProccessing/src/main/java/bg/softuni/json/dtos/product/ProductWithBayerNameDto.java
@@ -0,0 +1,19 @@
+package bg.softuni.json.dtos.product;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ProductWithBayerNameDto {
+    private String name;
+    private BigDecimal price;
+    private String buyerFirstName;
+    private String buyerLastName;
+}

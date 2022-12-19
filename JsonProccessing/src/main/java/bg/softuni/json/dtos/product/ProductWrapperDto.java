55a141e565f87f70649720e517e9e937294980ab
darthReader <ddimitrov10@gmail.com>
2022-12-19 14:35:24 +0200

    task enhancement

---
+++ JsonProccessing/src/main/java/bg/softuni/json/dtos/product/ProductWrapperDto.java
@@ -0,0 +1,20 @@
+package bg.softuni.json.dtos.product;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class ProductWrapperDto {
+    private Integer count;
+    private List<ProductImportDto> products;
+
+    public ProductWrapperDto(List<ProductImportDto> products) {
+        this.products = products;
+        this.count = products.size();
+    }
+}

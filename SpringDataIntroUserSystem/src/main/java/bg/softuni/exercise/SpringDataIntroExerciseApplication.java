cf22a5523e224bcc946f2e66ccb273c7db49eea6
darthReader <ddimitrov10@gmail.com>
2022-12-20 07:09:13 +0200

    task enhancement

---
+++ SpringDataIntroUserSystem/src/main/java/bg/softuni/exercise/SpringDataIntroExerciseApplication.java
@@ -0,0 +1,14 @@
+package bg.softuni.exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataIntroExerciseApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringDataIntroExerciseApplication.class, args);
+	}
+
+}

05410661ee0df3633e336fc5b602515ec6b710f0
darthReader <ddimitrov10@gmail.com>
2022-12-21 17:21:59 +0200

    task enhancement

---
+++ SpringDataIntroUserSystem/src/main/java/bg/softuni/exercise/repositories/TownRepository.java
@@ -0,0 +1,10 @@
+package bg.softuni.exercise.repositories;
+
+
+import bg.softuni.exercise.entities.Town;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TownRepository extends JpaRepository<Town, Long> {
+}

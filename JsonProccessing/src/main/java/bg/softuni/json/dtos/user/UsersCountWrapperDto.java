2e463f95773641b815c155edc67cd0cd2d7a9b6a
darthReader <ddimitrov10@gmail.com>
2022-12-21 17:23:38 +0200

    task enhancement

---
+++ JsonProccessing/src/main/java/bg/softuni/json/dtos/user/UsersCountWrapperDto.java
@@ -0,0 +1,20 @@
+package bg.softuni.json.dtos.user;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class UsersCountWrapperDto {
+    private Integer usersCount;
+    private List<UserWrapperDetailsDto> users;
+
+    public UsersCountWrapperDto(List<UserWrapperDetailsDto> users) {
+        this.users = users;
+        this.usersCount = users.size();
+    }
+}

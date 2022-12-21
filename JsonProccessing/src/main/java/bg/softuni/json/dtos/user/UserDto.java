b8d97857c4db715c925205bb6113c2b1147e7bde
darthReader <ddimitrov10@gmail.com>
2022-12-21 17:22:41 +0200

    task enhancement

---
+++ JsonProccessing/src/main/java/bg/softuni/json/dtos/user/UserDto.java
@@ -0,0 +1,49 @@
+package bg.softuni.json.dtos.user;
+
+import bg.softuni.json.dtos.product.ProductDto;
+import bg.softuni.json.dtos.product.ProductImportDto;
+import bg.softuni.json.dtos.product.ProductWrapperDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Set;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class UserDto {
+
+    private String firstName;
+    private String lastName;
+    private Integer age;
+    private Set<UserDto> friend;
+    private Set<ProductDto> soldProducts;
+    private Set<ProductDto> bought;
+
+    public String getFullName(){
+        return firstName + " " + lastName;
+    }
+
+    public UsersCountWrapperDto usersCountWrapperDto(List<UserDto> users){
+        List<UserWrapperDetailsDto> usersWrapper =
+                users
+                        .stream()
+                        .map(UserDto::userWrapperDetailsDto)
+                        .toList();
+
+        return new UsersCountWrapperDto(usersWrapper);
+    }
+
+    public UserWrapperDetailsDto userWrapperDetailsDto(){
+        List<ProductImportDto> products = soldProducts
+                .stream()
+                .map(ProductDto::ProductImportDto)
+                .toList();
+        ProductWrapperDto productWrapperDto = new ProductWrapperDto(products);
+        return new UserWrapperDetailsDto(firstName, lastName, age, productWrapperDto);
+    }
+}

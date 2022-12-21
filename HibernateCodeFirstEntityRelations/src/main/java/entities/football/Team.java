a0311a9f27679254ead835958d31e3e87ff753fb
darthReader <ddimitrov10@gmail.com>
2022-12-21 17:21:46 +0200

    task enhancement

---
+++ HibernateCodeFirstEntityRelations/src/main/java/entities/football/Team.java
@@ -0,0 +1,41 @@
+package entities.football;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import entities.football.Color;
+import java.math.BigInteger;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "teams")
+public class Team extends BaseEntity {
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String logo;
+
+    @Column(length = 4, nullable = false)
+    private String initials;
+
+    @ManyToOne
+    @JoinColumn
+    private Color primaryColor;
+
+    @ManyToOne
+    @JoinColumn
+    private Color secondaryColor;
+
+    @ManyToOne
+    private Town town;
+
+    @Column
+    private BigInteger budget;
+
+}

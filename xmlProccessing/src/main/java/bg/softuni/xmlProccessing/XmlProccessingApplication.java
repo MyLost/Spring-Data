22552482077daae0a3aedb16a3ff7d3271160de2
darthReader <ddimitrov10@gmail.com>
2022-12-29 09:47:35 +0200

    task enhancement

---
+++ xmlProccessing/src/main/java/bg/softuni/xmlProccessing/XmlProccessingApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.xmlProccessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlProccessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlProccessingApplication.class, args);
+	}
+
+}
